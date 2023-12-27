91d1f55d43585cb728c2cc763f9305276a1af310
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-12-27 15:13:17 +0100

    Add hyperparameter tuning

--- core/util.py
+++ core/util.py
@@ -1,15 +1,23 @@
 """Miscellaneous utility functions."""
 
+import copy
+import json
+from typing import Callable, Literal
 from keras.callbacks import (
     EarlyStopping,
     LearningRateScheduler,
     ReduceLROnPlateau,
-    Callback
+    Callback,
+    History
 )
 from keras.models import Model
+from keras.optimizers import Adam
+from keras_tuner import HyperParameters, Tuner
+from keras_tuner.src.engine.trial import Trial
 from sklearn.metrics import accuracy_score
 import numpy as np
 import numpy.typing as npt
+from core.model import new_SNN_classifier, new_SNN_encoder, new_Siamese_Network
 
 
 def new_default_callbacks(
@@ -150,3 +158,213 @@ def evaluate_classification(
     mean_acc = float(np.mean(it_acc))
 
     return it_y_true, it_y_pred, it_acc, mean_acc
+
+
+def new_SNN_builder(
+    mode: Literal["classifier", "siamese"],
+    loss: Callable | str,
+    metrics: list[str] | None,
+    num_classes: int | None = None,
+    distance: Callable | None = None
+):
+    """Create SNN builder for Keras Tuner.
+
+    Reference:
+        https://keras.io/guides/keras_tuner/getting_started/#tune-the-model-architecture
+    """
+
+    def __build_model(hp: HyperParameters, get_encoder: bool = False):
+        # Encoder part
+        snn_encoder = new_SNN_encoder(
+            dense_count=hp.Int("dense_count", min_value=2,
+                               max_value=10, step=2),
+            dense_base=hp.Int("dense_base", min_value=8, max_value=64, step=8),
+            dropout=hp.Boolean("encoder_dropout")
+        )
+
+        if mode == "classifier":
+            # Full classifier
+            assert num_classes is not None
+            model = new_SNN_classifier(
+                snn_encoder,
+                num_classes=num_classes,
+                dropout=hp.Boolean("decoder_dropout")
+            )
+        else:
+            # Siamese NN
+            model = new_Siamese_Network(
+                snn_encoder,
+                distance=distance,
+                batch_normalization=hp.Boolean("siamese_bn"),
+                sigmoid_output=hp.Boolean("siamese_dropout")
+            )
+
+        # Use Adam optimizer
+        adam = Adam(
+            learning_rate=hp.Float(
+                "start_lr", min_value=0.00005, max_value=0.001, step=0.00005
+            ),
+            beta_1=hp.Float(
+                "beta_1", min_value=0.7, max_value=0.9, step=0.1
+            ),
+            beta_2=hp.Float(
+                "beta_2", min_value=0.99, max_value=0.999, step=0.001
+            ),
+            epsilon=hp.Float(
+                "epsilon", min_value=0.001, max_value=0.01, step=0.001
+            )
+        )
+
+        # Compile model
+        model.compile(
+            loss=loss,
+            optimizer=adam,
+            metrics=metrics
+        )
+
+        if not get_encoder:
+            return model
+        else:
+            return snn_encoder, model
+
+    return __build_model
+
+
+class TunerHistory:
+    """Keep track of tuner history.
+
+    (Index histories by trial.)
+    """
+
+    __history: dict[tuple[str, str], History] = {}
+
+    def get_or_init_trial_history(
+        self,
+        trial_id: tuple[str, str],
+        model: Model
+    ) -> History:
+        """Get history for trial or initialize if not exists.
+
+        (Must provide `Model` in case trial history must be initialized.
+        -> Prevents error with Keras `History` callback.)
+        """
+        # Get history for exact trial
+        trial_history = self.__history.get(trial_id)
+
+        # Initialize trial history if exact history does not exist
+        if trial_history is None:
+            # Try finding a previous trial with same hyperparameter hash
+            trial_hp_hash = trial_id[1]
+            full_trial_ids = list(self.__history.keys())
+            hp_hashes = [id[1] for id in full_trial_ids]
+            # If exists base history of current trial off of previous trial
+            try:
+                prev_trial_idx = hp_hashes.index(trial_hp_hash)
+                prev_trial_id = full_trial_ids[prev_trial_idx]
+                trial_history = copy.copy(self.__history[prev_trial_id])
+            # Else create new history
+            except ValueError:
+                trial_history = History()
+                trial_history.on_train_begin()
+
+            trial_history.model = model
+            self.__history[trial_id] = trial_history
+
+        return trial_history
+
+    def get_history_by_trial_id(self, trial_id: str) -> History | None:
+        """Get history using only Keras tuner trial id."""
+        full_trial_ids = list(self.__history.keys())
+        trial_ids = [id[0] for id in full_trial_ids]
+        # Try finding matching trial
+        try:
+            trial_idx = trial_ids.index(trial_id)
+            full_trial_id = full_trial_ids[trial_idx]
+            return self.__history[full_trial_id]
+        except ValueError:
+            return None
+
+
+class TunerHistoryCallback(Callback):
+    """Hack to collect training history for Keras Tuner trials.
+
+    Usage:
+
+    ```python
+    # Instantiate for tuner
+    tuner_history_cb = TunerHistoryCallback(tuner)
+
+    # Use as search callback
+    tuner.search(
+        ...,
+        callbacks=[tuner_history_cb]
+    )
+    ```
+
+    Reference:
+        https://stackoverflow.com/a/70410522
+    """
+
+    # Keep track of histories for all tries
+    # Use class instead of dict, because tuner copies cb class for every trial
+    # -> Keep state across trials
+    __history: TunerHistory
+    __trial: Trial | None
+    __trial_id: tuple[str, str]
+
+    def __init__(self, tuner: Tuner):
+        """Create new TunerHistoryCallback."""
+        super().__init__()
+        self.__history = TunerHistory()
+        tuner.on_trial_begin = self.__on_trial_begin
+
+    def __hp_hash(self, hp: dict[str, int | float | bool]) -> str:
+        """Create hash for hyperparameter config."""
+        # Remove all tuner-specific values (unique for every trial)
+        hp = copy.copy(hp)
+        tuner_keys = [
+            key for key in hp.keys()
+            if "tuner" in key
+        ]
+        for tuner_key in tuner_keys:
+            del hp[tuner_key]
+
+        return json.dumps(hp)
+
+    def __full_trial_id(self, trial: Trial) -> tuple[str, str]:
+        """Get trial id and hash of hyperparameters for trial."""
+        trial_id = str(trial.trial_id)
+        hp_values = trial.hyperparameters.values
+        hp_hash = self.__hp_hash(hp_values)
+        return (trial_id, hp_hash)
+
+    def on_epoch_end(self, epoch, logs=None):
+        """On epoch end callback."""
+        # Ensure current trial is set
+        if self.__trial is None:
+            raise Exception("No trial set during epoch callback.")
+
+        # Get history for current trial
+        trial_history = self.__history.get_or_init_trial_history(
+            self.__trial_id,
+            self.model
+        )
+
+        # Forward epoch end callback to trial history
+        trial_history.on_epoch_end(epoch, logs)
+
+    def __on_trial_begin(self, trial: Trial):
+        """Sync. current trial.
+
+        Overwrite tuner `on_trial_begin`:
+
+        ```python
+        tuner.on_trial_begin = tuner_history_cb.__on_trial_begin
+        ```
+        """
+        self.__trial = trial
+        self.__trial_id = self.__full_trial_id(trial)
+
+    def get_trial_history(self, trial_id: str) -> History:
+        """Get history for trial."""
+        return self.__history.get_history_by_trial_id(str(trial_id))
